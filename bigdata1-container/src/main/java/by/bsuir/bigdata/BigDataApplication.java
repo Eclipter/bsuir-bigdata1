1cf58b03030a9e854c4788f89dc15ae9b08f28f3
Vladislav Dektiarev <eclipter97@gmail.com>
2017-11-19 02:13:13 -0800

    Add application class

---
+++ bigdata1-container/src/main/java/by/bsuir/bigdata/BigDataApplication.java
@@ -0,0 +1,14 @@
+package by.bsuir.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class BigDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataApplication.class, args);
+    }
+}
